8d507ee07b2177394ecaebfe85a6baa57cc241c2
alireza1233 <55350212+alireza1233@users.noreply.github.com>
2026-05-11 18:05:13 +0330

    Delete combine.bat

--- combine.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo This script will combine the split files to restore ACD.
-echo Make sure to run it in the same folder as the split files.
-echo.
-copy /b acd_parts.tar.gz.* acd_full.tar.gz
-echo Extraction completed. Run 'tar -xzf acd_full.tar.gz' if you have tar, or extract manually.
-pause
